cdf6c39e9d0e7421d29efb9a1dba44759f8ce96a
Jakub Okrasa <j.m.okrasa@gmail.com>
2020-12-03 10:00:31 +0100

    request basic permissions and show the map

--- app/src/main/java/pl/jakubokrasa/bikeroutes/ui/home/RecordRouteFragment.kt
+++ app/src/main/java/pl/jakubokrasa/bikeroutes/ui/home/RecordRouteFragment.kt
@@ -1,30 +1,107 @@
 package pl.jakubokrasa.bikeroutes.ui.home
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.preference.PreferenceManager.getDefaultSharedPreferences
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.views.MapView
 import pl.jakubokrasa.bikeroutes.R
+
 
 class RecordRouteFragment : Fragment() {
 
     private lateinit var recordRouteViewModel: RecordRouteViewModel
+    lateinit var map : MapView
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         recordRouteViewModel = ViewModelProvider(this).get(RecordRouteViewModel::class.java)
+        Configuration.getInstance().load(context, getDefaultSharedPreferences(context)) //osmdroid config
         val root = inflater.inflate(R.layout.fragment_record_route, container, false)
 //        val textView: TextView = root.findViewById(R.id.tv_record_route)
 //        recordRouteViewModel.text.observe(viewLifecycleOwner, Observer {
 //            textView.text = it
 //        })
+
+        map = root.findViewById(R.id.mv_record_route)
+        map.setTileSource(TileSourceFactory.MAPNIK)
+
+        requestPermissionsIfNecessary(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        )
+
+
         return root
     }
+
+    private fun requestPermissionsIfNecessary(permissions: Array<String>) {
+        val permissionsToRequest: ArrayList<String> = ArrayList()
+        for (perm in permissions) {
+            if (ContextCompat.checkSelfPermission(requireContext(), perm) != PackageManager.PERMISSION_GRANTED) { //todo check if context is from fragment (look at 2. comment https://stackoverflow.com/a/40760760/9343040)
+                permissionsToRequest.add(perm)
+            }
+        }
+        if (permissionsToRequest.isNotEmpty()) {
+            requestPermissions(
+                permissionsToRequest.toTypedArray(),
+                REQUEST_PERMISSIONS_REQUEST_CODE
+            )
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    override fun onPause() {
+        super.onPause()
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().save(this, prefs);
+        map.onPause() //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        val permissionsToRequest: ArrayList<String?> = ArrayList()
+        for (i in grantResults.indices) {
+            permissionsToRequest.add(permissions[i])
+        }
+        if (permissionsToRequest.isNotEmpty()) {
+            requestPermissions(
+                permissionsToRequest.toTypedArray(),
+                REQUEST_PERMISSIONS_REQUEST_CODE
+            )
+        }
+    }
+
+
+    companion object {
+        const val REQUEST_PERMISSIONS_REQUEST_CODE = 0
+    }
+
 }
