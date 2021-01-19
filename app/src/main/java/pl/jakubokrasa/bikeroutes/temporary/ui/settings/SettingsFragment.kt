6d90efaa2d805bab4e6bb2fd375cb2201b869872
Jakub Okrasa <j.m.okrasa@gmail.com>
2021-01-19 11:03:37 +0100

    settings fragment init (not finished)

--- app/src/main/java/pl/jakubokrasa/bikeroutes/temporary/ui/settings/SettingsFragment.kt
+++ app/src/main/java/pl/jakubokrasa/bikeroutes/temporary/ui/settings/SettingsFragment.kt
@@ -4,21 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.preference.PreferenceFragmentCompat
 import pl.jakubokrasa.bikeroutes.R
 
-class NotificationsFragment : Fragment() {
-
-
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        return root
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
     }
 }
