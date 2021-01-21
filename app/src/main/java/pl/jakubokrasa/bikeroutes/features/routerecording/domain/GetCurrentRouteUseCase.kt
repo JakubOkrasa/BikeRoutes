f7efccba360b39084e35b548c8e6f495c5a9eac0
Jakub Okrasa <j.m.okrasa@gmail.com>
2021-01-21 11:15:06 +0100

    AA: base use case

--- app/src/main/java/pl/jakubokrasa/bikeroutes/features/routerecording/domain/GetCurrentRouteUseCase.kt
+++ app/src/main/java/pl/jakubokrasa/bikeroutes/features/routerecording/domain/GetCurrentRouteUseCase.kt
@@ -4,18 +4,9 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import pl.jakubokrasa.bikeroutes.core.base.domain.UseCase
 import pl.jakubokrasa.bikeroutes.features.routerecording.CurrentRouteRepository
 
-class GetCurrentRouteUseCase(private val repository: CurrentRouteRepository) {
-    operator fun invoke(
-        scope: CoroutineScope,
-        onResult: (Result<Route>) -> Unit
-    ) {
-        scope.launch {
-            val result = withContext(Dispatchers.IO) {
-                runCatching { repository.getCurrentRoute() } //dzieki runcatching, w viemodelu bedzie mozna wywolac onSuccess / onFailure
-            }
-            onResult(result)
-        }
-    }
+class GetCurrentRouteUseCase(private val repository: CurrentRouteRepository): UseCase<Route, Unit>() {
+    override suspend fun action(params: Unit) = repository.getCurrentRoute()
 }
