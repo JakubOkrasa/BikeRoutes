34bf333fa9be58db554e3d1fca0a83e4f0e97920
Jakub Okrasa <j.m.okrasa@gmail.com>
2021-05-08 09:54:03 +0200

    batch writes - saveRoute

--- app/src/main/java/pl/jakubokrasa/bikeroutes/features/map/data/remote/RouteRepositoryImpl.kt
+++ app/src/main/java/pl/jakubokrasa/bikeroutes/features/map/data/remote/RouteRepositoryImpl.kt
@@ -13,17 +13,57 @@ import pl.jakubokrasa.bikeroutes.features.map.domain.model.Route
 class RouteRepositoryImpl(private val firestore: FirebaseFirestore): RouteRepository {
     override suspend fun addRoute(route: Route) {
 
-        //save RouteResponse
+//        //save RouteResponse
+//        val routeResponse = RouteResponse(route.userId, route.name, route.description, route.sharingType, route.distance)
+//        val routeId = firestore.collection("routes").add(routeResponse).await().id
+//
+//        //save List<PointResponse>
+//        val pointsMap = mapOf("pointsArray" to route.points.map {PointResponse(it)})
+//        firestore.collection("points").document(routeId).set(pointsMap).await()
+//
+//        //assign route to a user
+//        val userRef = firestore.collection("users").document(route.userId)
+//        userRef.update("routes", FieldValue.arrayUnion(routeId)).await()
+
+
+        // TRANSACTIONS DO NOT WORK OFFLINE | all inside worked
+//        firestore.runTransaction {
+//            transaction ->
+//
+//            //save RouteResponse
+//            val routeResponse = RouteResponse(route.userId, route.name, route.description, route.sharingType, route.distance)
+//            val routesRef = firestore.collection("routes")
+//            val routeDoc = routesRef.document()
+//            transaction.set(routeDoc, routeResponse)
+//
+//            //save Points
+//            val pointsMap = mapOf("pointsArray" to route.points.map {PointResponse(it)})
+//            val pointsDoc = firestore.collection("points").document(routeDoc.id)
+//            transaction.set(pointsDoc, pointsMap) // nie wiem czy mogę wziąć id z poprzedniej linii ( .id ), skoro to będzie się wykonywać współbieżnie
+//
+//            //assign route to a user
+//            val userDoc = firestore.collection("users").document(route.userId)
+//            transaction.update(userDoc, "routes", FieldValue.arrayUnion(routeDoc.id))
+//
+//        }.await()
+
         val routeResponse = RouteResponse(route.userId, route.name, route.description, route.sharingType, route.distance)
-        val routeId = firestore.collection("routes").add(routeResponse).await().id
-
-        //save List<PointResponse>
+        val routeDoc = firestore.collection("routes").document()
         val pointsMap = mapOf("pointsArray" to route.points.map {PointResponse(it)})
-        firestore.collection("points").document(routeId).set(pointsMap).await()
+        val pointsDoc = firestore.collection("points").document(routeDoc.id)
+        val userDoc = firestore.collection("users").document(route.userId)
 
-        //assign route to a user
-        val userRef = firestore.collection("users").document(route.userId)
-        userRef.update("routes", FieldValue.arrayUnion(routeId)).await()
+        //WRITE BATCHES WORK OFFLINE
+        firestore.runBatch { batch ->
+            //save RouteResponse
+            batch.set(routeDoc, routeResponse)
+
+            //save Points
+            batch.set(pointsDoc, pointsMap) // nie wiem czy mogę wziąć id z poprzedniej linii ( .id ), skoro to będzie się wykonywać współbieżnie
+
+            //assign route to a user
+            batch.update(userDoc, "routes", FieldValue.arrayUnion(routeDoc.id))
+        }.await()
 
     }
 
