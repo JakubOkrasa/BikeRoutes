54560360c06be1292e0e58bf59b3db91c49d29e5
Jakub Okrasa <j.m.okrasa@gmail.com>
2021-05-23 16:49:42 +0200

    removed intents from MainActivity + improved arch. (not tested)

---
+++ app/src/main/java/pl/jakubokrasa/bikeroutes/core/app/presentation/MainNavigator.kt
@@ -0,0 +1,25 @@
+package pl.jakubokrasa.bikeroutes.core.app.presentation
+
+import androidx.annotation.IdRes
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.fragment.findNavController
+import pl.jakubokrasa.bikeroutes.core.navigation.FragmentTransition
+import pl.jakubokrasa.bikeroutes.core.provider.ActivityProvider
+
+class MainNavigator(
+    private val activityProvider: ActivityProvider,
+    @IdRes private val navHostFragmentRes: Int
+    ) {
+
+    private fun getFragmentManager() =
+        (activityProvider.foregroundActivity as? FragmentActivity)?.supportFragmentManager
+
+    private fun getNavController() =
+        getFragmentManager()
+            ?.findFragmentById(navHostFragmentRes)
+            ?.findNavController()
+
+    fun navigateTo(destinationId: Int) {
+        getNavController()?.navigate(destinationId)
+    }
+}
--- app/src/main/java/pl/jakubokrasa/bikeroutes/core/di/NavigationModule.kt
+++ app/src/main/java/pl/jakubokrasa/bikeroutes/core/di/NavigationModule.kt
@@ -4,6 +4,7 @@ import androidx.navigation.navOptions
 import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 import pl.jakubokrasa.bikeroutes.R
+import pl.jakubokrasa.bikeroutes.core.app.presentation.MainNavigator
 import pl.jakubokrasa.bikeroutes.core.navigation.FragmentNavigator
 import pl.jakubokrasa.bikeroutes.core.navigation.FragmentNavigatorImpl
 import pl.jakubokrasa.bikeroutes.core.provider.ActivityProvider
@@ -20,6 +21,7 @@ val navigationModule = module {
         defaultNavOptions = get()
     ) }
 
+    single { MainNavigator(get(), navHostFragmentRes = R.id.nav_host_fragment)}
     single { UserNavigator(get())}
     single { MapFrgNavigator(get()) }
     single { MyRoutesNavigator(get()) }
