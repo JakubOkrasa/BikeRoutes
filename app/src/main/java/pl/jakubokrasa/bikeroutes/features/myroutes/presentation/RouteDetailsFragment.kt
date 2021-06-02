6ce58f1a6313f1792c2650b84de16dc55bef5e21
Jakub Okrasa <j.m.okrasa@gmail.com>
2021-06-02 15:20:36 +0200

    remove my location marker from routeDetailsFrg

--- app/src/main/java/pl/jakubokrasa/bikeroutes/features/myroutes/presentation/RouteDetailsFragment.kt
+++ app/src/main/java/pl/jakubokrasa/bikeroutes/features/myroutes/presentation/RouteDetailsFragment.kt
@@ -1,24 +1,13 @@
 package pl.jakubokrasa.bikeroutes.features.myroutes.presentation
 
-import android.app.Activity
 import android.app.Dialog
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
-import android.location.Location
 import android.os.Bundle
 import android.view.View
-import android.view.WindowManager
 import androidx.appcompat.widget.Toolbar
-import androidx.core.content.res.ResourcesCompat
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
-import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.CustomZoomButtonsController
-import org.osmdroid.views.overlay.Marker
 import org.osmdroid.views.overlay.Polyline
 import pl.jakubokrasa.bikeroutes.R
 import pl.jakubokrasa.bikeroutes.core.base.platform.BaseFragment
@@ -26,12 +15,9 @@ import pl.jakubokrasa.bikeroutes.core.extensions.hideKeyboard
 import pl.jakubokrasa.bikeroutes.core.extensions.makeGone
 import pl.jakubokrasa.bikeroutes.core.extensions.makeVisible
 import pl.jakubokrasa.bikeroutes.core.util.*
-import pl.jakubokrasa.bikeroutes.core.util.enums.MapMode
 import pl.jakubokrasa.bikeroutes.core.util.enums.sharingType
 import pl.jakubokrasa.bikeroutes.databinding.FragmentRouteDetailsBinding
 import pl.jakubokrasa.bikeroutes.features.common.presentation.CommonRoutesNavigator
-import pl.jakubokrasa.bikeroutes.features.map.domain.LocationService
-import pl.jakubokrasa.bikeroutes.features.map.presentation.MapFragment.Companion.SEND_LOCATION_ACTION
 import pl.jakubokrasa.bikeroutes.features.map.presentation.model.PointDisplayable
 import pl.jakubokrasa.bikeroutes.features.map.presentation.model.RouteDisplayable
 
@@ -39,25 +25,18 @@ import pl.jakubokrasa.bikeroutes.features.map.presentation.model.RouteDisplayabl
 class RouteDetailsFragment : BaseFragment<MyRoutesViewModel>(R.layout.fragment_route_details) {
 
     override val viewModel: MyRoutesViewModel by sharedViewModel()
-
     private var _binding: FragmentRouteDetailsBinding? = null
     private val binding get() = _binding!!
     private lateinit var route: RouteDisplayable
     private lateinit var points: List<PointDisplayable>
     private lateinit var polyline: Polyline
-    private val mLocalBR: LocalBroadcastManager by inject()
-    private lateinit var mPreviousLocMarker: Marker
     private lateinit var dialogConfirmRemove: Dialog
     private val navigator: CommonRoutesNavigator by inject()
-
-    private var mapMode = MapMode.moveFreely
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentRouteDetailsBinding.bind(view)
         configureOsmDroid(requireContext())
-        requireActivity().startService(Intent(context, LocationService::class.java))
-        LocationUtils(activity as Activity).enableGpsIfNecessary()
 
         updateToolbar()
         showRoute(view)
@@ -65,12 +44,6 @@ class RouteDetailsFragment : BaseFragment<MyRoutesViewModel>(R.layout.fragment_r
         binding.btFollow.setOnClickListener(btFollowOnClick)
 
         if(isMyRoute()) dialogConfirmRemove = DialogConfirm(requireContext(), "Are you sure to remove this route?", "remove")
-    }
-
-    override fun onStart() {
-        super.onStart()
-        val locFilter = IntentFilter(SEND_LOCATION_ACTION)
-        mLocalBR.registerReceiver(locationServiceReceiver, locFilter)
     }
 
     override fun onResume() {
@@ -81,12 +54,6 @@ class RouteDetailsFragment : BaseFragment<MyRoutesViewModel>(R.layout.fragment_r
     override fun onPause() {
         super.onPause()
         binding.mapView.onPause()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        mLocalBR.unregisterReceiver(locationServiceReceiver)
-        stopLocationService()
     }
 
     private fun showRoute(view: View) {
@@ -121,8 +88,6 @@ class RouteDetailsFragment : BaseFragment<MyRoutesViewModel>(R.layout.fragment_r
         }
     }
 
-    private fun isMyRoute() = arguments?.getBoolean(IS_MY_ROUTE_KEY) ?: false
-
 	private val toolbarIconsEditModeOnClick = Toolbar.OnMenuItemClickListener {
             insideMenuItem ->
         when (insideMenuItem.itemId) {
@@ -150,11 +115,6 @@ class RouteDetailsFragment : BaseFragment<MyRoutesViewModel>(R.layout.fragment_r
         route.name = binding.etRouteName.text.toString()
         route.description = binding.etRouteDescription.text.toString()
         if (binding.swPrivate.isChecked) route.sharingType = sharingType.PRIVATE else route.sharingType = sharingType.PUBLIC
-    }
-
-    private val btDialogConfirmOnClick = View.OnClickListener {
-        viewModel.removeRouteAndNavBack(route)
-        dialogConfirmRemove.dismiss()
     }
 
     private fun updateRouteInfoLayout() {
@@ -214,38 +174,6 @@ class RouteDetailsFragment : BaseFragment<MyRoutesViewModel>(R.layout.fragment_r
         binding.toolbar.menu.clear()
     }
 
-    private fun newLocationUpdateUI(geoPoint: GeoPoint) {
-        showCurrentLocationMarker(geoPoint)
-        if(mapMode == MapMode.followLocation) binding.mapView.controller.animateTo(geoPoint)
-        binding.mapView.invalidate()
-    }
-
-    private fun showCurrentLocationMarker(geoPoint: GeoPoint) {
-        val currentLocMarker = Marker(binding.mapView, context)
-        currentLocMarker.icon = ResourcesCompat.getDrawable(resources,
-            R.drawable.marker_my_location,
-            null)
-        currentLocMarker.position = GeoPoint(geoPoint)
-        currentLocMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER)
-        if(this::mPreviousLocMarker.isInitialized) binding.mapView.overlays.remove(
-            mPreviousLocMarker)
-        binding.mapView.overlays.add(currentLocMarker)
-        mPreviousLocMarker = currentLocMarker
-    }
-
-    private val locationServiceReceiver: BroadcastReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            val loc = intent!!.getParcelableExtra<Location>("EXTRA_LOCATION")
-            loc?.let {
-                newLocationUpdateUI(GeoPoint(loc))
-            }
-        }
-    }
-
-    private fun stopLocationService() {
-        requireActivity().stopService(Intent(requireContext(), LocationService::class.java))
-    }
-
     private fun setRoute() {
         arguments
             ?.getParcelable<RouteDisplayable>(ROUTE_TO_FOLLOW_KEY)
@@ -262,18 +190,20 @@ class RouteDetailsFragment : BaseFragment<MyRoutesViewModel>(R.layout.fragment_r
                 }
     }
 
+    private fun isMyRoute() = arguments?.getBoolean(IS_MY_ROUTE_KEY) ?: false
+
     private val btFollowOnClick = View.OnClickListener {
         navigator.openFollowRouteFragment(route, points)
     }
 
     override fun onPendingState() {
         super.onPendingState()
-        binding.progressLayout.visibility = View.VISIBLE
+        binding.progressLayout.makeVisible()
     }
 
     override fun onIdleState() {
         super.onIdleState()
-        binding.progressLayout.visibility = View.GONE
+        binding.progressLayout.makeGone()
     }
 
     companion object {
