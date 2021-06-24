06a4ca50130515af49a8742ca93614e8ed54f90d
Jakub Okrasa <j.m.okrasa@gmail.com>
2021-06-24 18:17:41 +0200

    distinct filters work (also in domain layer)

--- app/src/main/java/pl/jakubokrasa/bikeroutes/features/myroutes/domain/GetMyRoutesWithFilterUseCase.kt
+++ app/src/main/java/pl/jakubokrasa/bikeroutes/features/myroutes/domain/GetMyRoutesWithFilterUseCase.kt
@@ -1,6 +1,5 @@
 package pl.jakubokrasa.bikeroutes.features.myroutes.domain
 
-import android.util.Log
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -8,6 +7,7 @@ import kotlinx.coroutines.withContext
 import pl.jakubokrasa.bikeroutes.core.user.domain.UserAuth
 import pl.jakubokrasa.bikeroutes.features.common.domain.BoundingBoxData
 import pl.jakubokrasa.bikeroutes.features.common.domain.FilterData
+import pl.jakubokrasa.bikeroutes.features.common.domain.doesRouteCoversMap
 import pl.jakubokrasa.bikeroutes.features.map.domain.RemoteRepository
 import pl.jakubokrasa.bikeroutes.features.map.domain.model.Route
 
@@ -18,7 +18,7 @@ class GetMyRoutesWithFilterUseCase(
     suspend fun action(filterData: FilterData): List<Route> {
         var mapBB: BoundingBoxData? = null
         filterData.boundingBoxData?.let {
-            mapBB = if(it.latNorth == 0.0 && it.latSouth == 0.0 && it.lonEast == 0.0 && it.lonWest == 0.0) null
+            mapBB = if(isBoundingBoxNotInitialized(it)) null
             else it
         }
         val filteredList = remoteRepository.getMyRoutesWithFilter(auth.getCurrentUserId(), filterData)
@@ -43,43 +43,7 @@ class GetMyRoutesWithFilterUseCase(
             }
         }
 
-    private fun doesRouteCoversMap(routeBB: BoundingBoxData, mapBB: BoundingBoxData
-    ) = doesRouteCoversMapVertically(routeBB, mapBB) && doesRouteCoversMapHorizontally(routeBB, mapBB)
 
-    private fun doesRouteCoversMapHorizontally(
-        routeBB: BoundingBoxData, mapBB: BoundingBoxData
-    ) = doesRouteCoversMapAndBottomIsOutside(routeBB, mapBB) || doesRouteCoversMapAndTopIsOutside(
-            routeBB,
-            mapBB) || isRouteBetweenVertically(routeBB, mapBB)
-
-    private fun doesRouteCoversMapVertically(
-        routeBB: BoundingBoxData, mapBB: BoundingBoxData
-    ) = doesRouteCoversMapAndLeftIsOutside(routeBB, mapBB) || doesRouteCoversMapAndRightIsOutside(
-        routeBB, mapBB) || isRouteBetweenHorizontally(routeBB, mapBB)
-
-
-    private fun doesRouteCoversMapAndBottomIsOutside(
-        routeBB: BoundingBoxData, mapBB: BoundingBoxData
-    ) = (routeBB.latSouth < mapBB.latSouth && routeBB.latNorth > mapBB.latSouth)
-
-    private fun doesRouteCoversMapAndTopIsOutside(
-        routeBB: BoundingBoxData, mapBB: BoundingBoxData
-    ) = (routeBB.latNorth > mapBB.latNorth && routeBB.latSouth < mapBB.latNorth)
-
-    private fun isRouteBetweenVertically(
-        routeBB: BoundingBoxData, mapBB: BoundingBoxData
-    ) = (routeBB.latNorth < mapBB.latNorth && routeBB.latSouth > mapBB.latSouth)
-
-
-    private fun doesRouteCoversMapAndLeftIsOutside(
-        routeBB: BoundingBoxData, mapBB: BoundingBoxData
-    ) = (routeBB.lonWest < mapBB.lonWest && routeBB.lonEast > mapBB.lonWest)
-
-    private fun doesRouteCoversMapAndRightIsOutside(
-        routeBB: BoundingBoxData, mapBB: BoundingBoxData
-    ) = (routeBB.lonEast > mapBB.lonEast && routeBB.lonWest < mapBB.lonEast)
-
-    private fun isRouteBetweenHorizontally(
-        routeBB: BoundingBoxData, mapBB: BoundingBoxData
-    ) = (routeBB.lonEast < mapBB.lonEast && routeBB.lonWest > mapBB.lonWest)
+    private fun isBoundingBoxNotInitialized(it: BoundingBoxData) =
+        it.latNorth == 0.0 && it.latSouth == 0.0 && it.lonEast == 0.0 && it.lonWest == 0.0
 }
--- app/src/main/java/pl/jakubokrasa/bikeroutes/features/sharedroutes/domain/GetSharedRoutesWithFilterUseCase.kt
+++ app/src/main/java/pl/jakubokrasa/bikeroutes/features/sharedroutes/domain/GetSharedRoutesWithFilterUseCase.kt
@@ -5,7 +5,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import pl.jakubokrasa.bikeroutes.core.user.domain.UserAuth
+import pl.jakubokrasa.bikeroutes.features.common.domain.BoundingBoxData
 import pl.jakubokrasa.bikeroutes.features.common.domain.FilterData
+import pl.jakubokrasa.bikeroutes.features.common.domain.doesRouteCoversMap
 import pl.jakubokrasa.bikeroutes.features.map.domain.RemoteRepository
 import pl.jakubokrasa.bikeroutes.features.map.domain.model.Route
 
@@ -13,8 +15,20 @@ class GetSharedRoutesWithFilterUseCase(
     private val remoteRepository: RemoteRepository,
     private val auth: UserAuth
 ) {
-    suspend fun action(filterData: FilterData) =
-        remoteRepository.getSharedRoutesWithFilter(auth.getCurrentUserId(), filterData)
+    suspend fun action(filterData: FilterData): List<Route>  {
+        var mapBB: BoundingBoxData? = null
+        filterData.boundingBoxData?.let {
+            mapBB = if(isBoundingBoxNotInitialized(it)) null
+            else it
+        }
+        val filteredList = remoteRepository.getMyRoutesWithFilter(auth.getCurrentUserId(), filterData)
+        mapBB?.let {
+            return filteredList.filter { route ->
+                doesRouteCoversMap(route.boundingBoxData, mapBB!!)
+            }
+        }
+        return filteredList
+    }
 
     operator fun invoke(
         filterData: FilterData,
@@ -28,6 +42,9 @@ class GetSharedRoutesWithFilterUseCase(
                 onResult(result)
             }
         }
+
+private fun isBoundingBoxNotInitialized(it: BoundingBoxData) =
+    it.latNorth == 0.0 && it.latSouth == 0.0 && it.lonEast == 0.0 && it.lonWest == 0.0
 }
 
 
