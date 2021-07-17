33802de355371652ae6c73be8688cc9dc02bf884
Jakub Okrasa <j.m.okrasa@gmail.com>
2021-07-17 15:12:24 +0200

    File provider error - code commented and Local storage used for the time being

--- app/src/main/java/pl/jakubokrasa/bikeroutes/features/myroutes/domain/ExportRouteUseCase.kt
+++ app/src/main/java/pl/jakubokrasa/bikeroutes/features/myroutes/domain/ExportRouteUseCase.kt
@@ -3,7 +3,9 @@ package pl.jakubokrasa.bikeroutes.features.myroutes.domain
 import android.content.Context
 import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Build
 import android.os.Environment
+import androidx.core.content.FileProvider
 import kotlinx.coroutines.*
 import org.osmdroid.tileprovider.MapTileProviderBasic
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
@@ -11,6 +13,7 @@ import org.osmdroid.util.BoundingBox
 import org.osmdroid.views.Projection
 import org.osmdroid.views.drawing.MapSnapshot
 import org.osmdroid.views.overlay.Polyline
+import pl.jakubokrasa.bikeroutes.R
 import pl.jakubokrasa.bikeroutes.core.base.domain.UseCase
 import pl.jakubokrasa.bikeroutes.features.common.segments.domain.model.Segment
 import pl.jakubokrasa.bikeroutes.features.map.domain.model.Route
@@ -27,8 +30,13 @@ class ExportRouteUseCase(private val context: Context): UseCase<Uri, ExportRoute
     private fun getUri(
         snapshot: MapSnapshot, fileName: String
     ): Uri {
+
         val bitmap = Bitmap.createBitmap(snapshot.bitmap) //todo to powinno być w wątku Default
-        return saveImageToLocalStorage(bitmap, fileName)
+
+
+
+
+        return saveImageToLocalCache(bitmap, fileName)
     }
 
 
@@ -63,9 +71,11 @@ class ExportRouteUseCase(private val context: Context): UseCase<Uri, ExportRoute
                 0)).run()
     }
 
-    private fun saveImageToLocalStorage(bitmap: Bitmap, fileName: String): Uri {
+    private fun saveImageToLocalCache(bitmap: Bitmap, fileName: String): Uri {
             val uri: Uri
-//            try { //todo czy tu nie potrzebne try catch, bo do runCatching błąd trafi?
+    //todo czy tu potrzebne try catch, bo do runCatching błąd trafi?
+
+            // local storage only for API<24
             if(!isExternalStorageWritable()) throw Exception("External storage is not writable")
                 val file = File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES),
                     "$fileName.png")
@@ -73,13 +83,20 @@ class ExportRouteUseCase(private val context: Context): UseCase<Uri, ExportRoute
                 bitmap.compress(Bitmap.CompressFormat.PNG, 90, stream)
                 stream.close()
                 uri = Uri.fromFile(file)
-//            catch(IOException e) {
-//                Log.d(TAG, "IOException while trying to write file for sharing: " + e.getMessage());
-//            }
+
+        // file provider, doesn't work
+//        val imagesFolder = File(context.cacheDir, "images")
+//        imagesFolder.mkdirs()
+//        val file = File(imagesFolder, "$fileName.png")
+//        val stream = FileOutputStream(file)
+//        bitmap.compress(Bitmap.CompressFormat.PNG, 90, stream)
+//        stream.close()
+//        uri = FileProvider.getUriForFile(context, context.resources.getString(R.string.file_provider_path), file)
+
             return uri
     }
 
-    fun isExternalStorageWritable(): Boolean {
+    private fun isExternalStorageWritable(): Boolean {
         val state = Environment.getExternalStorageState()
         return Environment.MEDIA_MOUNTED == state
     }
